7971b846c11944b9c925c3e36447fcaeb064c18c
Dorson <dorson@live.com>
2023-08-25 15:14:35 -0400

    removed unnecessary functions

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -51,35 +51,7 @@ def get_dealers_from_cf(url, **kwargs):
   
     return results
 
-def get_dealers_by_state_from_cf(url, **kwargs):
-    results = []
-    # Call get_request with a URL parameter
-    json_result = get_request(url, st=st)
-    if json_result:
-        # Get the row list in JSON as dealers
-        dealers = json_result["dbs"]
-        # For each dealer object
-        for dealer in dealers:
-            # Get its content in `doc` object
-            dealer_doc = dealer["doc"]
-            # Create a CarDealer object with values in `doc` object
-            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
-                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
-                                   short_name=dealer_doc["short_name"],
-                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
-            results.append(dealer_obj)
-  
-    return results
 
-def get_dealer_from_cf_by_id(url, dealer_id):
-    json_result = get_request(url, id=dealer_id)
-    if json_result:
-        dealer = json_result["dbs"][0]
-        dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
-                               id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
-                               short_name=dealer["short_name"],
-                               st=dealer["st"], zip=dealer["zip"])
-    return dealer_obj
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
 # def get_dealer_by_id_from_cf(url, dealerId):
